d92179f73ed9b1bfefe52f0a6e1182e76db88302
dev <danielbenedidgarcia@gmail.com>
2024-01-25 01:24:55 +0100

    Metodos en CompanyService

--- src/main/java/Services/CompanyService.java
+++ src/main/java/Services/CompanyService.java
@@ -4,32 +4,70 @@ import dao.UserImplDAO;
 import model.Company;
 import model.JobOffer;
 import model.User;
+import org.checkerframework.checker.units.qual.C;
 
 public class CompanyService {
     public void createCompany(Company co){
         Company c = new Company();
         c.setName(String.valueOf(co));
-        CompanyImplDAO uid = new CompanyImplDAO();
+        CompanyImplDAO cid = new CompanyImplDAO();
         try {
-            uid.createCompany(c);
+            cid.createCompany(c);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     public void removeCompany(Company co){
-
+        Company c = new Company();
+        c.setName(String.valueOf(co));
+        CompanyImplDAO cid = new CompanyImplDAO();
+        try {
+            cid.removeCompany(c);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
     public Company createCompany(String name){
-
-        return null;
+        Company c = new Company();
+        c.setName(name);
+        CompanyImplDAO cid = new CompanyImplDAO();
+        try {
+            cid.createCompany(c);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return c;
     }
     public void printCompanyInfo(Company co){
-
+        Company c = new Company();
+        c.setName(String.valueOf(co));
+        CompanyImplDAO cid = new CompanyImplDAO();
+        try {
+            cid.updateCompany(c);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
     public Company createCompany(String name, String description){
-        return null;
+        Company c = new Company();
+        c.setName(name);
+        c.setDescription(description);
+        CompanyImplDAO cid = new CompanyImplDAO();
+        try {
+            cid.createCompany(c);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return c;
     }
     public void addJobOffer(Company co, JobOffer job){
-
+        Company c = new Company();
+        c.setName(String.valueOf(co));
+        CompanyImplDAO cid = new CompanyImplDAO();
+        try {
+            cid.getCandidaturesByJobOffer(co, job);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
